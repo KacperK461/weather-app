2929781a77adf274fd153540e325cacf3dcd7224
Kacper Kustra <k.kustra100@gmail.com>
2021-05-26 22:13:15 +0200

    Implement data context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DataContext } from 'context';
 import App from './App';
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataContext>
+      <App />
+    </DataContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
