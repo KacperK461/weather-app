805792f0db436ba953167045f18405f8a7e51fa3
Kacper Kustra <k.kustra100@gmail.com>
2021-05-26 22:00:28 +0200

    Add error handling functionality to useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,13 +3,27 @@ import { useState, useEffect } from 'react';
 const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
+  const [error, setError] = useState({ hasError: false, err: null });
 
   const getData = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
+    let data;
+
+    try {
+      const response = await fetch(url);
+      if (response.status >= 400)
+        throw new Error(`Error: ${response.status} when call: ${url}`);
+
+      data = await response.json();
+    } catch (err) {
+      setError({ hasError: true, err });
+      setLoading(false);
+
+      return false;
+    }
 
     setData(data);
     setLoading(false);
+
     return data;
   };
 
@@ -17,7 +31,7 @@ const useFetch = (url) => {
     if (url) getData(url);
   }, [url]);
 
-  return { loading, data, getData };
+  return { loading, data, error, getData };
 };
 
 export default useFetch;
